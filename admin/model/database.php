c7199b4a585fda5212f61a434208d8583d6e9792
Fin <tdtatum2@gmail.com>
2022-03-23 19:05:26 -0500

    Heroku - JawsDB.

--- admin/model/database.php
+++ admin/model/database.php
@@ -1,9 +1,10 @@
 <?php
 
-    $dsn = "mysql:host=localhost; dbname=zippyusedautos";
-    $username = 'root';
+    $dsn = "mysql:host=w3epjhex7h2ccjxx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com; dbname=ttba11idfgr9j59g";
+    $username = 'ssx8rouilpvwxdr5';
+    $password = 'w6x5rwplgay82ki7';
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error = 'Database Error: ' . $e->getMessage();
         echo $error;
